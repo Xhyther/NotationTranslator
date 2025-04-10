73002c559d77db19c8e02fb49a59e477315f8d84
Vhon Michael <vhontabalan@gmail.com>
2025-04-11 05:02:46 +0800

    Defining the root commands and the needed commands

--- NotationTranslator/Program.cs
+++ NotationTranslator/Program.cs
@@ -4,9 +4,27 @@ namespace NotationTranslator
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var rootCommand = new RootCommand("fixNotation");
+
+            //Need to define commands for translating between notations
+            //Infix to Postfix
+            //Infix to Prefix
+            //Prefix to Postfix
+
+            //Option or Arugment that reverses that Operation
+            //i.e. Postfix to Infix -f ==  Infix to Postfix
+
+
+
+
+            rootCommand.SetHandler(rootCommand =>
+            {
+                Console.WriteLine("fixNotation");
+            });
+
+            await rootCommand.InvokeAsync(args);
         }
     }
 }
