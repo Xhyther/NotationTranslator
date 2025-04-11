c84bc919cf70242458b58120ff2941bb54f25f0c
Vhon Michael <vhontabalan@gmail.com>
2025-04-11 19:56:41 +0800

    Added the option to the convert command for translation of the equation

--- NotationTranslator/Program.cs
+++ NotationTranslator/Program.cs
@@ -6,11 +6,41 @@ namespace NotationTranslator
     {
         static async Task Main(string[] args)
         {
-            var rootCommand = new RootCommand("ds2");
+            //Option for the notation to convert from
+            // has default value of infix
+            var fromOption = new Option<string>
+                (
+                name: "--from",
+                description: "The notation to convert from",
+                getDefaultValue: () => "infix"
+                );
+
+            //Aliases for the from option
+            fromOption.AddAlias("--f");
+            fromOption.AddAlias("-f");
+
+            //Option for the notation to convert to
+            // is required
+            var toOption = new Option<string>
+                 (
+                 name: "--to",
+                 description: "The notation to convert to"
+                 ){IsRequired = true};
+
+            //Aliases for the to option
+            toOption.AddAlias("--t");
+            toOption.AddAlias("-t");
+
+            //Register the from option with the comman
             var convertCommand = new Command("convert", "Convert notation from one format to another");
+ 
+            convertCommand.AddOption(fromOption);
+            convertCommand.AddOption(toOption);
 
 
 
+            var rootCommand = new RootCommand("ds2");
+            rootCommand.AddCommand(convertCommand);
 
             rootCommand.SetHandler(rootCommand =>
             {
