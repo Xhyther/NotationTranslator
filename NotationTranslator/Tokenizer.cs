352d8fca52ae94261a25dcdea301b18696f32bb5
Vhon Michael <vhontabalan@gmail.com>
2025-04-14 21:26:44 +0800

    added a list of tokens and their corresponding regex

--- NotationTranslator/Tokenizer.cs
+++ NotationTranslator/Tokenizer.cs
@@ -8,6 +8,21 @@ namespace NotationTranslator
 {
     public class Tokenizer
     {
+        private static readonly List<(TokenType Type, string Pattern)> TokenDefinitions = new()
+    {
+        (TokenType.Whitespace, @"^\s+"),
+        (TokenType.Number, @"^\d+(\.\d+)?"),
+        (TokenType.Identifier, @"^[a-zA-Z_][a-zA-Z0-9_]*"),
+        (TokenType.Plus, @"^\+"),
+        (TokenType.Minus, @"^-"),
+        (TokenType.Multiply, @"^\*"),
+        (TokenType.Divide, @"^/"),
+        (TokenType.Power, @"^\^"),
+        (TokenType.LeftParen, @"^\("),
+        (TokenType.RightParen, @"^\)"),
+        (TokenType.Comma, @"^,"),
+        (TokenType.Equals, @"^=")
+    };
 
     } 
 }
