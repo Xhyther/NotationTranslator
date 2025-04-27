030bc390b72d9cc8d680399eb167857c40dff75c
Vhon Michael <vhontabalan@gmail.com>
2025-04-28 00:19:53 +0800

    Added a basic Node model

--- NotationTranslator/Models/Node.cs
+++ NotationTranslator/Models/Node.cs
@@ -2,6 +2,15 @@
 {
     public class Node
     {
-       
+        public char Data { get; set; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+
+        public Node(char data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
     }
 }
